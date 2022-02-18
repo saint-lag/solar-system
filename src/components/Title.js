bb3f912436f79b4825cb0bd3f3965fe9138badfd
saint-lag <maiasena.gabriel@gmail.com>
2022-02-18 16:09:24 -0300

    [Requisito 3] Componente Title

---
+++ src/components/Title.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Title extends React.Component {
+  render() {
+    const { headline } = this.props;
+    return (
+      <h2>{ headline }</h2>
+    );
+  }
+}
+
+Title.propTypes = {
+  headline: PropTypes.string,
+};
+
+Title.defaultProps = {
+  headline: 'headline',
+};
+
+export default Title;
